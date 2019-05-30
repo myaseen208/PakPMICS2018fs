1f617f7eabc2a15ca8e5f474f382c6749f488a7b
Muhammad Yaseen <myaseen208@gmail.com>
2019-05-30 05:07:35 +0500

    First Commit

--- R/PakPMICS2018fs.R
+++ R/PakPMICS2018fs.R
@@ -1,10 +1,16 @@
-#' @title Multiple Indicator Cluster Survey (MICS) 2018 Child Questionnaire Data for Punjab, Pakistan
+#' @title Multiple Indicator Cluster Survey (MICS) 2018 Children Age 5-17 Questionnaire Data for Punjab, Pakistan
 #' @name  PakPMICS2018fs
 #' @docType data
 #' @keywords datasets
 #' @usage data(PakPMICS2018fs)
-#' @description \code{PakPMICS2018fs} data of Child questionnaire, MICS Punjab, 2014.
-#' @format A data frame with 157899 rows and 62 variables:
+#' @description \code{PakPMICS2018fs} provides data set and function for exploration of Multiple Indicator Cluster Survey (MICS) 2017-18 Children Age 5-17 questionnaire data for Punjab, Pakistan. The results of the present survey are critically important for the purposes of SDG monitoring, as the survey produces information on 32 global SDG indicators. The data was collected from 53,840 households selected at the second stage with systematic random sampling out of a sample of 2,692 clusters selected using Probability Proportional to size sampling. Six questionnaires were used in the survey:
+#' 1. a household questionnaire to collect basic demographic information on all de jure household members (usual residents), the household, and the dwelling;
+#' 2. a water quality testing questionnaire administered in three households in each cluster of the sample;
+#' 3. a questionnaire for individual women administered in each household to all women age 15-49 years;
+#' 4. a questionnaire for individual men administered in every second household to all men age 15-49 years;
+#' 5. an under-5 questionnaire, administered to mothers (or caretakers) of all children under 5 living in the household; and
+#' 6. a questionnaire for children age 5-17 years, administered to the mother (or caretaker) of one randomly selected child age 5-17 years living in the household.
+#' @format A data frame with 37052 rows and 276 variables:
 #' \describe{
 #'    \item{\code{HH1}}{Cluster number}
 #'    \item{\code{HH2}}{Household number}
